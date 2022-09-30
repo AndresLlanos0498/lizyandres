e3b5444cdccf33a3d41d1ae6ecefe8b47e5b0751
AndresLlanos0498 <andres.enrique98@gmail.com>
2022-09-30 11:42:13 -0500

    Updates

---
+++ precache-manifest.6d38e1360fee624526444d7496dc2fc7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd3905fd35d1ad5c6a48c9f721895eb",
+    "url": "/lizyandres/index.html"
+  },
+  {
+    "revision": "f07c6eaaa0be59f7ac47",
+    "url": "/lizyandres/static/css/main.d1e78bf8.chunk.css"
+  },
+  {
+    "revision": "61b18a9061daebe60b58",
+    "url": "/lizyandres/static/js/2.fa2a86ad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lizyandres/static/js/2.fa2a86ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07c6eaaa0be59f7ac47",
+    "url": "/lizyandres/static/js/main.4e11ca05.chunk.js"
+  },
+  {
+    "revision": "8466c5b31e122a313ef9",
+    "url": "/lizyandres/static/js/runtime-main.e0bebd06.js"
+  },
+  {
+    "revision": "f42807c11b0bd8a82d141e99232e8a1d",
+    "url": "/lizyandres/static/media/icon-amazon.f42807c1.png"
+  },
+  {
+    "revision": "262e5be1ba67422687dc70b101d0291a",
+    "url": "/lizyandres/static/media/icon-netflix.262e5be1.png"
+  }
+]);
